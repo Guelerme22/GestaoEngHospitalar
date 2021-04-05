e97f99925f7e95173b2f9ed70dbd4aeac5eb1fe3
Martacioli <martatacioli1@gmail.com>
2021-04-05 20:36:51 -0300

    Qr code

---
+++ src/Component/GerarQr.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import React from 'react';
+
+const GerarQr = ({idInventario, tamanho}) => {
+
+    return (
+        <div className="col-lg-12">
+            <img src= {`https://api.qrserver.com/v1/create-qr-code/?size=${tamanho}x${tamanho}&data=${idInventario}`}></img>
+         </div>
+    );
+};
+
+export default GerarQr;
--- src/Views/PaginaDoInventario.js
+++ src/Views/PaginaDoInventario.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import axios from 'axios'
 import { Redirect } from 'react-router';
-
+import GerarQr from '../Component/GerarQr'
 class PaginaDoInventario extends React.Component {
 
-  componentDidMount (){
-      this.consutarInventario()
+  state = {
+    inventario :                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       {}
+  }
+  componentDidMount() {
+    this.consutarInventario()
   }
 
-  consutarInventario =() =>{
-    axios.get(`https://engenharia-gestao-hospitalar.herokuapp.com/inventario/${this.props.match.params.id}`).then(response => {
-            this.setState({
-                inventario: response.data
-            })
-        }).catch(error => {
 
-        })
+  consutarInventario = () => {
+    axios.get(`https://engenharia-gestao-hospitalar.herokuapp.com/inventario/${this.props.match.params.id}`).then(response => {
+      this.setState({
+        inventario: response.data
+      })
+    }).catch(error => {
+
+    })
   }
 
   render() {
 
 
     return (<div>
-      
-      pagina do inventario {this.props.match.params.id}
+      <div className="col-lg-12" style={{ marginTop: "30px", marginBottom: "30px" }}>
+        <h2> INVENTÁRIO {this.props.match.params.id}</h2>
+        <hr></hr>
+
+      </div>
+      <div className="row" style={{ marginBottom: '100px' }}>
+        <div className="col-lg-6">
+
+          <GerarQr idInventario={this.props.match.params.id} tamanho="400"></GerarQr>
+        </div>
+        <div className="col-lg-6">
+          Página do inventário 
+          {this.state.inventario.descricao}
+
+        </div>
+      </div>
     </div>
     );
   }
